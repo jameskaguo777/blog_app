ec5141f914a9b2f1ae58d7f0de357bd6bd239f07
jameskaguo777 <jameskaguo@live.com>
2021-05-15 22:50:08 +0300

    typo fix

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: MyHomePage(title: 'Flutter Demo Home Page'),
-      initialRoute: '/',git 
+      initialRoute: '/',
       routes: {
         '/': (context) => HomePage(),
       },
