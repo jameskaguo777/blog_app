c71a1397cf35ce93c7c975c2568f675eab587469
jameskaguo777 <jameskaguo@live.com>
2021-05-17 01:44:56 +0300

    image slider

---
+++ lib/constants/demo_data.dart
@@ -0,0 +1,9 @@
+const Map<String, dynamic> CHALLECHES = {
+  'challenges': [
+    'https://kleinchik.com/737faa30.jpg',
+    'https://i.pinimg.com/originals/ff/cc/f8/ffccf879889e37d434cb5a96e7c166b6.jpg',
+    'https://www.familyhandyman.com/wp-content/uploads/2019/07/toilet-1-scaled-e1619716607755.jpg',
+    'https://pbs.twimg.com/media/EM6UnjWXkAEE1RD.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Australian_Toilet.JPG/1200px-Australian_Toilet.JPG'
+  ]
+};
--- lib/tabs/my_votes.dart
+++ lib/tabs/my_votes.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:v2/constants/demo_data.dart';
 
 class MyVotes extends StatefulWidget {
   _MyVotes createState() => _MyVotes();
 }
 
 class _MyVotes extends State<MyVotes> {
+  PageController _pictureSliderPageController =
+      PageController(initialPage: 0, keepPage: true);
+  int pictureSliverPageViewIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,25 +23,86 @@ class _MyVotes extends State<MyVotes> {
   }
 
   Widget _voting() => Container(
-    width: double.infinity,
-    padding: const EdgeInsets.all(8.0),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('Challenge:'),
-        Container(
-          width: double.infinity,
-          margin: const EdgeInsets.fromLTRB(0, 8.0, 0, 8.0),
-          padding: const EdgeInsets.all(8.0),
-          child: Text('To what percent is this school toilet clean? 2019'),
-          color: Colors.white,
+        width: double.infinity,
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Challenge:'),
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.fromLTRB(0, 8.0, 0, 8.0),
+              padding: const EdgeInsets.all(8.0),
+              child: Text('To what percent is this school toilet clean? 2019'),
+              color: Colors.white,
+            ),
+            _pictureSlider()
+          ],
         ),
-        Wrap(
-          direction: Axis.vertical,
-          children: [],
-        ),
-      ],
-    ),
-  );
+      );
+
+  Widget _pictureSlider() {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width,
+            child: PageView.builder(
+                itemCount: CHALLECHES['challenges'].length,
+                pageSnapping: true,
+                scrollDirection: Axis.horizontal,
+                controller: _pictureSliderPageController,
+                itemBuilder:
+                    (BuildContext context, pictureSliverPageViewIndex) {
+                  return Image.network(
+                    CHALLECHES['challenges'][pictureSliverPageViewIndex],
+                    loadingBuilder: (context, widget, events) {
+                      return events == null
+                          ? widget
+                          : Center(
+                              child: Text(
+                                  'Downloaded ${events.cumulativeBytesLoaded}/${events.expectedTotalBytes}'),
+                            );
+                    },
+                  );
+                }),
+          ),
+          Row(
+            children: [
+              IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    if (CHALLECHES['challenges'].length >=
+                            pictureSliverPageViewIndex &&
+                        pictureSliverPageViewIndex != 0) {
+                      pictureSliverPageViewIndex--;
+                      _pictureSliderPageController.animateToPage(
+                          pictureSliverPageViewIndex,
+                          duration: Duration(microseconds: 1000),
+                          curve: Curves.easeIn);
+                    }
+                  }),
+              IconButton(
+                  icon: Icon(Icons.arrow_forward),
+                  onPressed: () {
+                    if (CHALLECHES['challenges'].length >
+                        pictureSliverPageViewIndex) {
+                      pictureSliverPageViewIndex++;
+                      _pictureSliderPageController.animateToPage(
+                          pictureSliverPageViewIndex,
+                          duration: Duration(microseconds: 1000),
+                          curve: Curves.easeIn);
+                      print(pictureSliverPageViewIndex);
+                    } else {
+                      print(pictureSliverPageViewIndex);
+                    }
+                  }),
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
